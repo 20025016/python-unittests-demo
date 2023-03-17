131dda64cc2faa1502a5a7c6122bf8d51d04a290
Joshua Harry Ryan-Sprowson <20025016@tafe.wa.edu.au>
2023-03-17 13:54:05 +0800

    Add two numbers

---
+++ add.py
@@ -0,0 +1,13 @@
+"""Add two numbers together"""
+
+def add(first_number: int, second_number: int) -> int:
+    """Add two numbers togther
+    
+    args:
+        x: A first number
+        y: A second number
+
+    Returns:
+        total number
+    """
+    return first_number + second_number
