15fb08f7602893e1a651d414c49b94a5b2e658fe
Joshua Harry Ryan-Sprowson <20025016@tafe.wa.edu.au>
2023-03-17 14:12:08 +0800

    Add parameters validation

--- add.py
+++ add.py
@@ -1,5 +1,8 @@
 """Add two numbers together"""
 
+from exceptions import InvalidArgumentException
+
+
 def add(first_number: int, second_number: int) -> int:
     """Add two numbers togther
     
@@ -10,4 +13,6 @@ def add(first_number: int, second_number: int) -> int:
     Returns:
         total number
     """
+    if first_number is not int or second_number is not int:
+        raise InvalidArgumentException("An integer is expected")
     return first_number + second_number
